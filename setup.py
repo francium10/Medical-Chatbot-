f849c37861b820c4dda6c6bbf1320f74006cbfb3
francium10 <francis4lufwendo@gmail.com>
2025-10-05 17:40:04 -0400

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages,setup 
+
+setup(
+    name='Medical-Chatbot',
+    version='0.1.0',
+    author='Francis Lufwendo',
+    author_email='francis4lufwendo@gmail.com',
+    description='A medical chatbot application using LangChain and Flask',
+    packages=find_packages(),
+    install_requires=[]
+    )
