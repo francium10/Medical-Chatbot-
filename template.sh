39f0ae443e51c6efde97b5c04f5b85037fa86c2b
francium10 <francis4lufwendo@gmail.com>
2025-10-05 16:36:07 -0400

    Added Folder Structure

---
+++ template.sh
@@ -0,0 +1,16 @@
+#Create project directories/folders
+mkdir -p src
+mkdir -p research
+
+#Create files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch research/trials.ipynb
+touch .env
+touch setup.py
+touch app.py
+touch requirements.txt
+touch LICENSE
+
+echo "Project/file structure created successfully!."
